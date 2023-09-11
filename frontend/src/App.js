ef7cbf966928e681c1acea78909816c65674cbcd
brown <mosesbinkat@gmail.com>
2023-09-11 14:48:44 +0100

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/signup' element={<Signup />}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
