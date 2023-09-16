12e3310f43fdbf2c18dc1eefb94e94820ce8e276
brown <mosesbinkat@gmail.com>
2023-09-16 18:56:34 +0100

    commit

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,18 +1,20 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await signup(email, password)
     }
 
     return ( 
         <div className="container">
-            <form classname='signup' onSubmit={handleSubmit}>
+            <form className='signup' onSubmit={handleSubmit}>
             <h3>Sign up</h3>
 
             <label>Email:</label>
@@ -28,7 +30,8 @@ const Signup = () => {
             value={password}
             />
 
-            <button>Sign up</button>
+            <button disabled={isLoading}>Sign up</button>
+            {error && <div className='error'>{error}</div>}
         </form>
         </div>
      );
