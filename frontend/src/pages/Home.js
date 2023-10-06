c7a2d265b7aebaac7182cc1936edbbbc473de97b
brown <mosesbinkat@gmail.com>
2023-10-06 19:04:44 +0100

    fetching data from api

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
+    const [goods, setGoods] = useState()
+
+    useEffect(() => {
+        const fetchGoods = async () => {
+            const response = await fetch('/api/goods')
+            const json = await response.json()
+
+            if (response.ok) {
+                setGoods(json)
+            }
+        }
+
+        fetchGoods()
+    }, [])
+
     return ( 
-        <h1>Buy - Sell</h1>
+        <div className='goods'>
+            {goods && goods.map((goods) => (
+                <p key={goods._id}>{goods.name}</p>
+            ))}
+        </div>
         
      );
 }
