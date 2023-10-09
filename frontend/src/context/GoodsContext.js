0d0da46cdb964730406bd33880ef60e5ce9f59d7
brown <mosesbinkat@gmail.com>
2023-10-09 05:52:33 +0100

    goods context

---
+++ frontend/src/context/GoodsContext.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from "react";
+
+export const GoodsContext = createContext()
+
+export const goodsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_GOODS':
+            return {
+                goods: action.payload
+            }
+        case 'CREATE_GOODS':
+            return {
+                goods: [action.payload, ...state.goods]
+            }
+        default:
+            return state
+    }
+}
+
+export const GoodsContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(goodsReducer, {
+        goods: null
+    })
+    
+    return (
+        <GoodsContext.Provider value={{state, dispatch}}>
+
+        </GoodsContext.Provider>
+    )
+}
